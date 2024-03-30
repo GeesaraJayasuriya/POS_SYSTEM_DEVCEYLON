33b046d3e878549c381eb268ecd50391e477d059
GeesaraJayasuriya <geesarajayasuriya@gmail.com>
2024-03-30 21:54:58 +0530

    Initial commit

---
+++ src/com/dev_ceylon/pos_system/controllers/LoginFormController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+
+public class LoginFormController {
+
+    public void loginOnAction(ActionEvent actionEvent) {
+
+        System.out.println("My Name is Geesara");
+    }
+}
