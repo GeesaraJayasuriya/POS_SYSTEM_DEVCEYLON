00bf044e6d295b83b71d5f547632757c5bb1d484
GeesaraJayasuriya <geesarajayasuriya@gmail.com>
2024-03-31 16:56:26 +0530

    date- 0002

---
+++ src/com/dev_ceylon/pos_system/entity/SuperEntity.java
@@ -0,0 +1,8 @@
+package com.dev_ceylon.pos_system.entity;
+
+import java.io.Serializable;
+
+public  interface SuperEntity extends Serializable {
+
+
+}
---
+++ src/com/dev_ceylon/pos_system/entity/UserRole.java
@@ -0,0 +1,29 @@
+package com.dev_ceylon.pos_system.entity;
+
+import lombok.*;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRole implements SuperEntity {
+    @Id
+    @Column(name = "property_id")
+    private Integer propertyId;
+    @Column(name = "role_name",nullable = false)
+    private String  roleName;
+    @Column(name = "role_description",nullable = false)
+    private String roleDescription;
+
+    @OneToMany(mappedBy = "userRoles")
+    private User user;
+
+}
